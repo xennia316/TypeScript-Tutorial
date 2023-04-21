6f6c96bbbd0ea5644ac38df1d0f552ce8d3b7b22
xennia316 <xennia316@gmail.com>
2023-04-21 10:01:47 +0100

    objects and arrays

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,19 @@
-var circumference = function (diameter) {
-    var pi = Math.PI;
-    var result = diameter * pi;
-    return result;
+var names = ['Amehmbo', 'Ngewung', 'Sonia'];
+names.unshift("Aya");
+// names.push(2);
+//  Error message for the above line: Argument of type 'number' is not assignable to parameter of type 'string'. 5 names.push(2);
+console.log(names);
+var fruits = {
+    best: "mango",
+    common: "pineapple",
+    cheap: 100
 };
-console.log(circumference(3));
+// fruits = {
+//     best: "mango",
+//     common: "papaya"
+// }
+// fruits = {
+//     best: "mango",
+//     mostCommon: "pineapple",
+//     cheap: 100
+// }
--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,25 @@
-const circumference = (diameter: number) => {
- let pi = Math.PI;
- let result = diameter * pi;
 
- return result;
+let names = ['Amehmbo', 'Ngewung', 'Sonia'];
+
+names.unshift("Aya");
+// names.push(2);
+//  Error message for the above line: Argument of type 'number' is not assignable to parameter of type 'string'. 5 names.push(2);
+
+console.log(names)
+
+let fruits = {
+    best: "mango",
+    common: "pineapple",
+    cheap: 100
 }
 
-console.log(circumference(3));
+// fruits = {
+//     best: "mango",
+//     common: "papaya"
+// }
+
+// fruits = {
+//     best: "mango",
+//     mostCommon: "pineapple",
+//     cheap: 100
+// }
