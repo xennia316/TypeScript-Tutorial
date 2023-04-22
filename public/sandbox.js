04a7e61ee595b5f68f57dd0aca0be18967e396a2
xennia316 <xennia316@gmail.com>
2023-04-22 13:56:25 +0100

    lesson 7: rearranged folder structure

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,9 +1,10 @@
-var names = ['Amehmbo', 'Ngewung', 'Sonia'];
+"use strict";
+let names = ['Amehmbo', 'Ngewung', 'Sonia'];
 names.unshift("Aya");
 // names.push(2);
 //  Error message for the above line: Argument of type 'number' is not assignable to parameter of type 'string'. 5 names.push(2);
 console.log(names);
-var fruits = {
+let fruits = {
     best: "mango",
     common: "pineapple",
     cheap: 100
@@ -21,21 +22,21 @@ var fruits = {
 // let firstArray: string[];
 // firstArray.push('no');
 // console.log(firstArray)
-var sampleArray = [];
+let sampleArray = [];
 sampleArray.push('yes');
 console.log(sampleArray);
 // Union types
-var mixedArrays = [];
+let mixedArrays = [];
 mixedArrays.push("Heyyyy");
 mixedArrays.push(12);
 // mixedArrays.push(false)
 // Different ways to declare the object type: 
 // You can use the direct declaration method like this:
-var ninja1 = {};
+let ninja1 = {};
 // Or explicitly
-var ninja2;
+let ninja2;
 // Type any 
-var age = 1;
+let age = 1;
 age = "Hello";
 console.log("My age is ", age);
 age = true;
