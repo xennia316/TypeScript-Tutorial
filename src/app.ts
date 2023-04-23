5f8f6c88baf35d12f7af33b0541253d31d9fe16f
xennia316 <xennia316@gmail.com>
2023-04-23 09:58:19 +0100

    dom manipulation

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,14 @@
-// let greeting: Function
+const anchor = document.querySelector('a');
 
-// example1
-let greeting: (a:string, b:string) => void;
+console.log(anchor?.href);
 
-greeting =  (name: string, words: string) => {
-    console.log(`${name} says ${words}`);
-}
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
-// example2 
-let calc: (a: number, b: number, c: string) => number;
-calc = (numOne: number, numTwo: number, operation: string): number => {
-    if(operation === "add"){
-        return (numOne + numTwo)
-    }
-    else if (operation === "subtract"){
-        return numOne - numTwo;
-    }
-    else if(operation === "multiply"){
-        return (numOne * numTwo)
-    }
-    else if(operation === "divide"){
-        return (numOne / numTwo)
-    }
-    else{
-        return 0
-    }
-}
+console.log(form)
 
-// example 3
-let logDetails: (obj: {name: string, age: number}) => void;
+// Input
 
-type person = {name: string, age: number}
-
-logDetails = (ninja: person) => {
-    console.log(`${ninja.name} is ${ninja.age} years old`)
-}
+const type = document.querySelector("#type") as HTMLSlotElement;
+const tofrom = document.querySelector("#toform") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLSlotElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
