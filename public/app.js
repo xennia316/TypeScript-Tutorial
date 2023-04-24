f3d8c9cda4057a5fa796e3dc9717458555676bfa
xennia316 <xennia316@gmail.com>
2023-04-24 10:26:20 +0100

    lesson 12: typescript with classes

--- public/app.js
+++ public/app.js
@@ -8,3 +8,22 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#toform");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// Class
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `Hello ${this.client}, you owe $${this.amount} for ${this.details}`;
+    }
+}
+const firstInvoice = new Invoice("Luigi", "work", 300);
+const secondInvoice = new Invoice("Sonia", "school", 350);
+console.log(firstInvoice);
+let invoices = [];
+// invoices.push("Heyoo")
+firstInvoice.format();
+invoices.push(firstInvoice, secondInvoice);
+console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -12,3 +12,34 @@ const type = document.querySelector("#type") as HTMLSlotElement;
 const tofrom = document.querySelector("#toform") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLSlotElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// Class
+
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `Hello ${this.client}, you owe $${this.amount} for ${this.details}`
+    }
+}
+
+const firstInvoice = new Invoice("Luigi", "work", 300);
+const secondInvoice = new Invoice("Sonia", "school", 350);
+
+console.log(firstInvoice);
+
+let invoices: Invoice[] = [];
+// invoices.push("Heyoo")
+
+firstInvoice.format();
+
+invoices.push(firstInvoice, secondInvoice);
+console.log(invoices);
