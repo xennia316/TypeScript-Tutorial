043d53ce7befcb3d7d7eaa3c224f834dac7f8454
xennia316 <xennia316@gmail.com>
2023-04-25 11:00:07 +0100

    lesson14: importing class invoice

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,4 @@
-const anchor = document.querySelector('a');
-
-console.log(anchor?.href);
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
-// console.log(form)
-
-// Input
-
-const type = document.querySelector("#type") as HTMLSlotElement;
-const tofrom = document.querySelector("#toform") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLSlotElement;
-const amount = document.querySelector("#amount") as HTMLInputElement;
-
-// Class
-// ********Defining a class Method 1*******
-// class Invoice {
-//    readonly client: string;
-//    private details: string;
-//    public amount: number;
-
-//     constructor(c: string, d: string, a: number){
-//         this.client = c;
-//         this.details = d;
-//         this.amount = a;
-//     }
-
-//     format(){
-//         return `Hello ${this.client}, you owe $${this.amount} for ${this.details}`
-//     }
-// }
-
-// ********Defining a class Method 1*******
-
-class Invoice {
-
-constructor( readonly client: string, private details: string, public amount: number ){}
-    format(){
-        return `Hello ${this.client}, you owe $${this.amount} for ${this.details}`
-    }
-}
-
+import {Invoice} from "./classes/invoice"
 const firstInvoice = new Invoice("Luigi", "work", 300);
 const secondInvoice = new Invoice("Sonia", "school", 350);
 
