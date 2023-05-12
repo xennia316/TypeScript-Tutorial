7e6d0a0fe4295b2bab39d3c4b67862d58089283a
xennia316 <xennia316@gmail.com>
2023-05-12 17:42:38 +0100

    lesson 9: example2

--- public/sandbox.js
+++ public/sandbox.js
@@ -5,3 +5,23 @@ let greeting;
 greeting = (name, words) => {
     console.log(`${name} says ${words}`);
 };
+// example2 
+let calc;
+calc = (numOne, numTwo, operation) => {
+    if (operation === "add") {
+        return (numOne + numTwo);
+    }
+    else if (operation === "subtract") {
+        return numOne - numTwo;
+    }
+    else if (operation === "multiply") {
+        return (numOne * numTwo);
+    }
+    else if (operation === "divide") {
+        return (numOne / numTwo);
+    }
+    else {
+        return 0;
+    }
+};
+// example 3
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -6,3 +6,25 @@ let greeting: (a:string, b:string) => void;
 greeting =  (name: string, words: string) => {
     console.log(`${name} says ${words}`);
 }
+
+// example2 
+let calc: (a: number, b: number, c: string) => number;
+calc = (numOne: number, numTwo: number, operation: string): number => {
+    if(operation === "add"){
+        return (numOne + numTwo)
+    }
+    else if (operation === "subtract"){
+        return numOne - numTwo;
+    }
+    else if(operation === "multiply"){
+        return (numOne * numTwo)
+    }
+    else if(operation === "divide"){
+        return (numOne / numTwo)
+    }
+    else{
+        return 0
+    }
+}
+
+// example 3
